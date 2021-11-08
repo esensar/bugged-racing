479522d2922b1f51284aa0aa1f1d707911bbd95c
Ensar Sarajčić <es.ensar@gmail.com>
2021-11-08 19:53:23 +0100

    Add vehicle sounds and adjust RPM

--- vehicles/buggy.gd
+++ vehicles/buggy.gd
@@ -14,18 +14,27 @@ export (Curve) var steer_curve = null
 var steer_target = 0.0
 var steer_angle = 0.0
 
-export (float) var MAX_ENGINE_FORCE = 50.0
+export (float) var MAX_ENGINE_FORCE = 85.0
 export (float) var MAX_BRAKE_FORCE = 50.0
-export (float) var THROTTLE_POWER = 10000.0
+export (float) var THROTTLE_POWER = 6000.0
 export (float) var MAX_RPM_LOSS_PS = 3000.0
 
-export (Array) var gear_ratios = [ 6, 5, 4, 3, 2, 1.0 ]
-export (float) var reverse_ratio = -6.0
-export (float) var final_drive = 3.38
-export (float) var max_rpm = 7000
+export (Array) var gear_ratios = [ 3.4, 2.5, 2.0, 1.5, 1.25 ]
+export (float) var reverse_ratio = -3
+export (float) var final_drive = 3.45
+export (float) var max_rpm = 3500
 export (float) var min_rpm = 900
 export (float) var gear_switch_time = 0.2
 export (Curve) var power_curve = null
+
+
+var idle_sound_start: float = 0.5
+var idle_sound_end: float = 1
+
+var throttle_sound_start: float = 2.5
+var throttle_sound_end: float = 3.5
+
+var sound_time_left: float = 0.0
 
 var clutch_position: float = 0.0
 var rpm = 0
@@ -38,12 +47,18 @@ onready var flwheel: VehicleWheel = $front_left
 onready var rrwheel: VehicleWheel = $rear_right
 onready var rlwheel: VehicleWheel = $rear_left
 
+onready var engine_sound_player: AudioStreamPlayer3D = $engine_sound_player
+onready var engine_sound_playback: AudioStreamPlayback = $engine_sound_player.get_stream_playback()
+
 var traction_wheels: Array
 
 func _ready():
 	for wheel in [frwheel, flwheel, rrwheel, rlwheel]:
 		if wheel.use_as_traction:
 			traction_wheels.append(wheel)
+
+	_generate_engine_sound(0)
+	engine_sound_player.play()
 
 func _get_gear_ratio():
 	if gear == 0:
@@ -90,9 +105,9 @@ func _physics_process(delta: float):
 
 	if gear != 0:
 		rpm = lerp(rpm, transmission_rpm, delta * (1 - clutch_position))
-		rpm = _lerp_rpm(rpm, min_rpm, delta, delta * clutch_position * 3)
+		rpm = _lerp_rpm(rpm, min_rpm, delta, delta * clutch_position)
 	else:
-		rpm = _lerp_rpm(rpm, min_rpm, delta, delta * 3)
+		rpm = _lerp_rpm(rpm, min_rpm, delta, delta)
 	if _has_traction():
 		rpm += throttle * delta * clutch_position * THROTTLE_POWER
 	else:
@@ -100,6 +115,8 @@ func _physics_process(delta: float):
 	rpm = clamp(rpm, 0, max_rpm)
 	var rpm_factor = clamp(rpm / max_rpm, 0.0, 1.0)
 	var power_factor = power_curve.interpolate_baked(rpm_factor)
+
+	_generate_engine_sound(rpm_factor)
 
 	# Transfer to transmission
 	var transmission_input = power_factor * (1 - clutch_position) * _get_gear_ratio()
@@ -114,7 +131,7 @@ func _physics_process(delta: float):
 	rlwheel.brake = handbrake * MAX_BRAKE_FORCE / 2
 
 	var speed = wheel_rpm * 2.0 * PI * rrwheel.wheel_radius / 60.0 * 3600.0 / 1000.0
-	$Info.text = "Gear: %d, KPH: %.0f, RPM: %.0f, TRPM: %.0f, Engine force: %.0f" % [ gear, speed, rpm, transmission_rpm, engine_force ]
+	$Info.text = "Gear: %d, KPH: %.0f, RPM: %.0f, WheelRPM: %.0f, FinalRPM: %.0f, TRPM: %.0f, Engine force: %.0f" % [ gear, speed, rpm, wheel_rpm, final_rpm, transmission_rpm, engine_force ]
 
 	var steering_input = Input.get_action_strength("steer_left") - Input.get_action_strength("steer_right")
 	if abs(steering_input) < 0.05:
@@ -129,3 +146,12 @@ func _physics_process(delta: float):
 
 	steer_angle = steering_input * lerp(max_steer_angle_rad, speed_steer_angle_rad, steer_speed_factor)
 	steering = steer_angle
+
+func _generate_engine_sound(rpm_factor):
+	engine_sound_player.pitch_scale = 0.5 + rpm_factor
+	var to_fill = engine_sound_playback.get_frames_available()
+	var factor = rpm_factor
+	while to_fill > 0:
+		engine_sound_playback.push_frame(Vector2(1.0, 1.0) * factor)
+		factor += cos(factor) * sin(factor)
+		to_fill -= 1

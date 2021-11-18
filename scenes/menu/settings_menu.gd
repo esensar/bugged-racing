00ae651966a9b067a5f4014323c42ba2ed13c3fb
Ensar Sarajčić <es.ensar@gmail.com>
2021-11-18 20:05:35 +0100

    Add more options in the settings menu

--- scenes/menu/settings_menu.gd
+++ scenes/menu/settings_menu.gd
@@ -10,6 +10,8 @@ onready var music_slider: HSlider = $MarginContainer/VSplitContainer/CenterConta
 onready var auto_clutch_cb: CheckBox = $MarginContainer/VSplitContainer/CenterContainer/VBoxContainer/AutoClutchCheckBox
 onready var automatic_transmission_cb: CheckBox = $MarginContainer/VSplitContainer/CenterContainer/VBoxContainer/AutomaticTransmissionCheckBox
 onready var debug_cb: CheckBox = $MarginContainer/VSplitContainer/CenterContainer/VBoxContainer/DebugModeCheckBox
+onready var fullscreen_cb: CheckBox = $MarginContainer/VSplitContainer/CenterContainer/VBoxContainer/FullscreenCheckBox
+onready var borderless_cb: CheckBox = $MarginContainer/VSplitContainer/CenterContainer/VBoxContainer/BorderlessCheckBox
 
 func _ready() -> void:
 	master_slider.value = db2linear(AudioServer.get_bus_volume_db(master_bus))
@@ -18,6 +20,12 @@ func _ready() -> void:
 	debug_cb.pressed = GlobalSettings.debug
 	auto_clutch_cb.pressed = GlobalSettings.auto_clutch
 	automatic_transmission_cb.pressed = GlobalSettings.automatic_transmission
+	fullscreen_cb.pressed = false
+	borderless_cb.pressed = false
+	if OS.is_window_fullscreen():
+		_set_fullscreen(true)
+	if OS.get_borderless_window():
+		_set_borderless(true)
 
 
 func _on_debug_toggled(new_state: bool) -> void:
@@ -26,6 +34,16 @@ func _on_debug_toggled(new_state: bool) -> void:
 
 func _on_autoclutch_toggled(new_state: bool) -> void:
 	GlobalSettings.auto_clutch = new_state
+
+
+func _on_fullscreen_toggled(new_state: bool) -> void:
+	_set_borderless(false)
+	_set_fullscreen(new_state)
+
+
+func _on_borderless_toggled(new_state: bool) -> void:
+	_set_fullscreen(false)
+	_set_borderless(new_state)
 
 
 func _on_automatictransmission_toggled(new_state: bool) -> void:
@@ -47,3 +65,21 @@ func _on_SoundEffectsSlider_value_changed(new_value: float) -> void:
 
 func _on_MusicSlider_value_changed(new_value: float) -> void:
 	AudioServer.set_bus_volume_db(music_bus, linear2db(new_value))
+
+
+func _set_fullscreen(new_state: bool) -> void:
+	OS.set_window_fullscreen(new_state)
+	OS.set_window_maximized(new_state)
+	if new_state == true:
+		borderless_cb.disabled = true
+	else:
+		borderless_cb.disabled = false
+
+
+func _set_borderless(new_state: bool) -> void:
+	OS.set_borderless_window(new_state)
+	OS.set_window_maximized(new_state)
+	if new_state == true:
+		fullscreen_cb.disabled = true
+	else:
+		fullscreen_cb.disabled = false

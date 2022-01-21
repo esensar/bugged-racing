8a41653dbbdebba4d6b15ab28b19b9892f5cbd71
Ensar Sarajčić <es.ensar@gmail.com>
2022-01-21 16:55:27 +0100

    Prevent multiple gear switches on single action with manual transmission

--- player/vehicle_controller.gd
+++ player/vehicle_controller.gd
@@ -6,8 +6,7 @@ export(NodePath) var vehicle_path = null
 onready var _vehicle: BuggedVehicle = get_node(vehicle_path)
 
 
-func _input(_event: InputEvent) -> void:
-	# Using _input just to make sure events are captured right away
+func _physics_process(_delta: float) -> void:
 	_vehicle.inputs.throttle = Input.get_action_strength("throttle")
 	_vehicle.inputs.clutch = Input.get_action_strength("clutch")
 	_vehicle.inputs.brake = Input.get_action_strength("brake")
@@ -22,7 +21,7 @@ func _input(_event: InputEvent) -> void:
 	_vehicle.inputs.steering = steering_input
 	if Input.is_action_just_pressed("gear_up"):
 		_vehicle.inputs.gear_request = BuggedVehicle.GearRequest.UP
-	elif Input.is_action_pressed("gear_down"):
+	elif Input.is_action_just_pressed("gear_down"):
 		_vehicle.inputs.gear_request = BuggedVehicle.GearRequest.DOWN
 	else:
 		_vehicle.inputs.gear_request = BuggedVehicle.GearRequest.NONE
